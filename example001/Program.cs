648a823c40d49231e40d7ddc384078fe06b99d51
Anastasia <super.okay335@yandex.ru>
2022-08-28 20:07:59 +0300

    добавили решение 1 задачи

---
+++ example001/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int num = new Random().Next(100,1000);
+// Console.WriteLine(num);
+// int result = (num/10)%10;
+// Console.WriteLine(result);
+
+int num;
+ Console.WriteLine("Введите трехзначное число");
+num = Convert.ToInt32(Console.ReadLine());
+ int result = (num/10)%10;
+Console.WriteLine(result);
